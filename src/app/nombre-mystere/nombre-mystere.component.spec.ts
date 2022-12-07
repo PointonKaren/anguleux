51e1c04148b5d31c0bc94840b80d904025ed60e1
Karen Pointon <karen.pointon.webdev@gmail.com>
2022-12-07 15:43:52 +0100

    Correction des erreurs liés au changement difficulty -> intro

--- src/app/nombre-mystere/nombre-mystere.component.spec.ts
+++ src/app/nombre-mystere/nombre-mystere.component.spec.ts
@@ -1,11 +1,11 @@
-import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NombreMystereComponent } from './nombre-mystere.component';
 import { NmBetComponent } from './nm-bet/nm-bet.component';
 import { NmIntroComponent } from './nm-intro/nm-intro.component';
 import { NmGameComponent } from './nm-game/nm-game.component';
 
-describe('Test de la fonction showDifficulty du nombre mystère', () => {
+describe('Test de la fonction showIntro du nombre mystère', () => {
   let component: NombreMystereComponent;
   let fixture: ComponentFixture<NombreMystereComponent>;
 
@@ -24,25 +24,25 @@ describe('Test de la fonction showDifficulty du nombre mystère', () => {
     fixture.detectChanges();
   });
 
-  it('Le clic sur la span a bien déclenché la fonction showDifficulty()', () => {
-    spyOn(component, 'showDifficulty');
-    let tab = fixture.debugElement.nativeElement.querySelector('#difficulty');
+  it('Le clic sur la span a bien déclenché la fonction showIntro()', () => {
+    spyOn(component, 'showIntro');
+    let tab = fixture.debugElement.nativeElement.querySelector('#intro');
     tab.click();
-    expect(component.showDifficulty).toHaveBeenCalled();
+    expect(component.showIntro).toHaveBeenCalled();
   });
 
   it("L'onglet 'Pari' n'est pas visible", () => {
-    component.showDifficulty();
+    component.showIntro();
     expect(component.bet).toBeFalsy();
   });
 
   it("L'onglet 'Difficulté' est visible", () => {
-    component.showDifficulty();
-    expect(component.difficulty).toBeTruthy();
+    component.showIntro();
+    expect(component.intro).toBeTruthy();
   });
 
   it("L'onglet 'Jeu' n'est pas visible", () => {
-    component.showDifficulty();
+    component.showIntro();
     expect(component.game).toBeFalsy();
   });
 });
@@ -80,7 +80,7 @@ describe('Test de la fonction showBet du nombre mystère', () => {
 
   it("L'onglet 'Difficulté' n'est pas visible", () => {
     component.showBet();
-    expect(component.difficulty).toBeFalsy();
+    expect(component.intro).toBeFalsy();
   });
 
   it("L'onglet 'Jeu' n'est pas visible", () => {
@@ -122,7 +122,7 @@ describe('Test de la fonction showGame du nombre mystère', () => {
 
   it("L'onglet 'Difficulté' n'est pas visible", () => {
     component.showGame();
-    expect(component.difficulty).toBeFalsy();
+    expect(component.intro).toBeFalsy();
   });
 
   it("L'onglet 'Jeu' est visible", () => {
