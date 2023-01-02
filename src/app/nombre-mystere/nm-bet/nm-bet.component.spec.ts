ac7925787377951b9e65f1e44fb51c931e3b653f
Karen Pointon <karen.pointon.webdev@gmail.com>
2023-01-02 12:05:34 +0100

    test sur l'input du pari fonctionnel

--- src/app/nombre-mystere/nm-bet/nm-bet.component.spec.ts
+++ src/app/nombre-mystere/nm-bet/nm-bet.component.spec.ts
@@ -3,7 +3,34 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NmBetComponent } from './nm-bet.component';
 
-describe('Test de la fonction limitFunction', () => {
+describe('Test de la fonction betFunction', () => {
+  let component: NmBetComponent;
+  let fixture: ComponentFixture<NmBetComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NmBetComponent],
+      imports: [ReactiveFormsModule],
+    }).compileComponents();
+    fixture = TestBed.createComponent(NmBetComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Le nombre de tentatives noté dans le formulaire est bien conservé', () => {
+    spyOn(component, 'betFunction');
+    component.harderPlease = true;
+    fixture.detectChanges();
+    const betInput =
+      fixture.debugElement.nativeElement.querySelector('#betInput');
+    betInput.value = 42;
+    betInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(component.tries.value).toBe(42);
+  });
+});
+
+describe("Test de l'appel à la fonction betFunction", () => {
   let component: NmBetComponent;
   let fixture: ComponentFixture<NmBetComponent>;
 
