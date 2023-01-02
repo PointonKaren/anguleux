1f42a165f5e1a5fec7837bf0a2378bae1108a876
Karen Pointon <karen.pointon.webdev@gmail.com>
2023-01-02 10:30:12 +0100

    corrections mineures + tests unitaires bet-component

--- src/app/nombre-mystere/nm-bet/nm-bet.component.spec.ts
+++ src/app/nombre-mystere/nm-bet/nm-bet.component.spec.ts
@@ -2,22 +2,25 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NmBetComponent } from './nm-bet.component';
 
-describe('NmBetComponent', () => {
+describe('Test de la fonction limitFunction', () => {
   let component: NmBetComponent;
   let fixture: ComponentFixture<NmBetComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NmBetComponent ]
-    })
-    .compileComponents();
+      declarations: [NmBetComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NmBetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Le clic sur le bouton Envoyer a bien déclenché la fonction betFunction()', () => {
+    spyOn(component, 'betFunction');
+    let tab = fixture.debugElement.nativeElement.querySelector('#betButton');
+    tab.click();
+    expect(component.betFunction).toHaveBeenCalled();
+    // Test échoué, essayer de comprendre pourquoi
   });
 });
