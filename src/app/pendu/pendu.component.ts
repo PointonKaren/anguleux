83636875990296491c108aaa48b3f5bfe2c552d8
Karen Pointon <karen.pointon.webdev@gmail.com>
2023-01-09 17:22:44 +0100

    Préparation du site [En cours de dev] pour déploiement

--- src/app/pendu/pendu.component.ts
+++ src/app/pendu/pendu.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pendu',
   templateUrl: './pendu.component.html',
-  styleUrls: ['./pendu.component.scss']
+  styleUrls: ['./pendu.component.scss'],
 })
 export class PenduComponent {
-
+  constructor(title: Title, meta: Meta) {
+    title.setTitle('Anguleux - Pendu');
+    meta.updateTag({
+      name: 'description',
+      content:
+        'Petit jeu qui consiste à deviner un mot, en un nombre limité de tentatives.',
+    });
+  }
 }
--- src/app/tetris/tetris.component.ts
+++ src/app/tetris/tetris.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tetris',
   templateUrl: './tetris.component.html',
-  styleUrls: ['./tetris.component.scss']
+  styleUrls: ['./tetris.component.scss'],
 })
 export class TetrisComponent {
-
+  constructor(title: Title, meta: Meta) {
+    title.setTitle('Anguleux - Tetris');
+    meta.updateTag({
+      name: 'description',
+      content:
+        "Petit jeu de puzzle qui consiste à aligner horizontalement des pièces de différentes formes qui tombent depuis le haut de l'écran",
+    });
+  }
 }
