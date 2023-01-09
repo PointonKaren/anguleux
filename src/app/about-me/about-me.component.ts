3cc1f7ca9bc04d9e34fba9ca46bdcb0015a576c5
Karen Pointon <karen.pointon.webdev@gmail.com>
2023-01-09 17:06:54 +0100

    Mise en place de la page "About me"

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
 })
 export class AboutMeComponent {
-
+  constructor(title: Title, meta: Meta) {
+    title.setTitle('Anguleux - À propos');
+    meta.updateTag({
+      name: 'description',
+      content: "À propos d'Anguleux, me contacter et consulter mon portfolio.",
+    });
+  }
+  dob = new Date('10/03/1988');
+  today = new Date();
+  age = this.today.getFullYear() - this.dob.getFullYear();
 }
